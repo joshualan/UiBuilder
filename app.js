49be61f1da4e3c152f90a01838956f6c9249b8d9
joshualan <joshualan23@yahoo.com>
2016-06-08 12:52:02 -0400

    Simplified login() handlers and use authentications instead of sessions

--- app.js
+++ app.js
@@ -3,16 +3,14 @@
 
     /**
      * @ngdoc overview
-     * @name SSO-0506
+     * @name test1
      * @description
      * #
      *
      * Main module of the application.
-     * whenshaw temporary comment: Stage one for SSO -- connect/log in to data providers on demand,
-     *     rather than all at once when the app starts
      */
     angular
-        .module('SSO-0506', [
+        .module('test1', [
             'ngAnimate',
             'ngCookies',
             'ngResource',
@@ -37,7 +35,8 @@
         })
         .run(function ($q, $http, $rootScope, $state, $window, loginModal) {
             var dataProviders,
-                sessions = {};
+                sessions = {},
+                authentications = {};
 
             function getProviders() {
                 return $q(function (resolve, reject) {
@@ -56,10 +55,8 @@
             }
 
             $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
-                 var dataProviders = toState.data ? toState.data.ensureJsdos : null,
-                 loginSuccessHandler,
-                 loginFailHandler;
-                 
+                 var dataProviders = toState.data ? toState.data.ensureJsdos : null;
+
                 // TODO: IMPORTANT: Authenticaton logic must change to handle mutiple providers.
                 if (!dataProviders || !dataProviders.length || sessions[dataProviders[0]]) {
                     return;
@@ -67,7 +64,6 @@
 
                 event.preventDefault();
 
-         // whenshaw  The code that Alan and Wayne changed begins here, ends at line 147
                 // TODO: IMPORTANT: Must also handle fail.
                 getProviders()
                     .then(function (providers) {
@@ -76,9 +72,10 @@
                             authProviderConfig,
                             authProviderInstance,
                             jsdoSession,
+                            deferred = $.Deferred(),
                             loginSuccessHandler = function (jsdosession) {
                                 return jsdosession.addCatalog(provider.catalogUris[0]);
-                            }, 
+                            },
                             addCatalogSuccessHandler = function() {
                                         sessions[dataProviders[0]] = true;
                                         $state.go(toState.name, toParams);
@@ -98,24 +95,19 @@
                                     throw new Error("unexpected error initializing session manager");
                                 }
                             };
-                            
-                            
-                            
+                                                    
                         if (provider.authenticationModel === progress.data.Session.AUTH_TYPE_SSO) {
                             // has the data provider's AuthenticationProvider already been created?
-                            if (sessions[provider.authenticationProvider]) {
+                            if (authentications[provider.authenticationProvider]) {
                                 // set the provider option-object's authImpl property so it has the AuthenticationProvider object
                                 provider.authImpl = {                    
-                                    provider: sessions[provider.authenticationProvider]                    
+                                    provider: authentications[provider.authenticationProvider]                    
                                 };
                                 
                                 jsdoSession = new progress.data.JSDOSession(provider);
                                 
-                                jsdoSession.login(null, null, provider)
-                                .then(loginSuccessHandler)
-                                .then(addCatalogSuccessHandler)
-                                .then(undefined, errorHandler);
-                                
+                                deferred.resolve(jsdoSession.login(null, null, provider));
+ 
                             } else {
                                 authProviderConfig = providers[provider.authenticationProvider];
                                 authProviderInstance = 
@@ -130,18 +122,14 @@
                                     
                                     authProviderInstance.authenticate(result.email, result.password)
                                     .then(function (apInstance, result, info) {
-                                        sessions[provider.authenticationProvider] = apInstance;
+                                        authentications[provider.authenticationProvider] = apInstance;
                                         provider.authImpl = {    
                                             provider: apInstance
                                         };
+                                        
                                         jsdoSession = new progress.data.JSDOSession(provider);
-                                        return jsdoSession.login(null, null, provider);
-                                    })
-                                    .then(loginSuccessHandler)
-                                    .then(addCatalogSuccessHandler)
-                                    .then(undefined,
-                                          errorHandler
-                                    );
+                                        deferred.resolve(jsdoSession.login(null, null, provider));
+                                    });
                                 })
                                 .catch(function (reason) {
                                     console.log(reason);
@@ -152,81 +140,28 @@
                             
                             loginModal()
                             .then(function (result) {
-                                return jsdoSession.login(result.email, result.password);
-                            })
+                                deferred.resolve(jsdoSession.login(result.email, result.password));
+                            });
+                        }
+                        
+                        deferred.then(function(loginPromise) {
+                            loginPromise
                             .then(loginSuccessHandler)
                             .then(addCatalogSuccessHandler)
-                            .then(undefined, errorHandler)
-                            .catch(function (reason) {
-                                console.log(reason);
-                            });
-                
-                        }
+                            .then(undefined, errorHandler);
+                        });
                 });
             });
         })
-         // whenshaw  The line above is the end of the code that Alan and Wayne modified
         .config(function ($stateProvider, $urlRouterProvider) {
             $stateProvider
                 .state('default', {
                     abstract:true,
                     url: '',
                     views: {
+                        'header': {"templateUrl":"components/static-header/template.html","controller":"StaticHeaderCtrl"},
+                        'main-navigation': {"templateUrl":"components/side-navigation/template.html","controller":"SideNavigationCtrl"},
                         
-                        'header': {
-                            templateUrl: 'components/header/template.html',
-                            controller: 'HeaderCtrl'
-                        },
-                        
-                        'side-navigation': {
-                            templateUrl: 'components/side-navigation/template.html',
-                            controller: 'SideNavigationCtrl'
-                        }
-                        
-                    }
-                })
-                .state('default.bin', {
-                    url: '/bin',
-                    views: {
-                        'content@': {
-                            templateUrl: 'modules/bin/views/index.html',
-                            controller: 'BinCtrl'
-                        }
-                    },
-                    resolve: {
-                        loadModule: ['$ocLazyLoad', function($ocLazyLoad) {
-                            return $ocLazyLoad.load('modules/bin/controllers/index.js');
-                        }]
-                    }
-                })
-                .state('default.bin.bin-view', {
-                    url: '/bin-view',
-                    templateUrl: 'modules/bin/views/bin-view.html',
-                    controller: 'BinBinViewCtrl',
-                    data: {
-                        ensureJsdos: ["Bin"]
-                    }
-                })
-                .state('default.customer', {
-                    url: '/customer',
-                    views: {
-                        'content@': {
-                            templateUrl: 'modules/customer/views/index.html',
-                            controller: 'CustomerCtrl'
-                        }
-                    },
-                    resolve: {
-                        loadModule: ['$ocLazyLoad', function($ocLazyLoad) {
-                            return $ocLazyLoad.load('modules/customer/controllers/index.js');
-                        }]
-                    }
-                })
-                .state('default.customer.customer-view', {
-                    url: '/customer-view',
-                    templateUrl: 'modules/customer/views/customer-view.html',
-                    controller: 'CustomerCustomerViewCtrl',
-                    data: {
-                        ensureJsdos: ["SSOwh0331"]
                     }
                 })
                 .state('default.dashboard', {
@@ -243,26 +178,34 @@
                         }]
                     }
                 })
-                .state('default.employee', {
-                    url: '/employee',
+                .state('default.mod1', {
+                    url: '/mod-1',
                     views: {
                         'content@': {
-                            templateUrl: 'modules/employee/views/index.html',
-                            controller: 'EmployeeCtrl'
+                            templateUrl: 'modules/mod-1/views/index.html',
+                            controller: 'Mod1Ctrl'
                         }
                     },
                     resolve: {
                         loadModule: ['$ocLazyLoad', function($ocLazyLoad) {
-                            return $ocLazyLoad.load('modules/employee/controllers/index.js');
+                            return $ocLazyLoad.load('modules/mod-1/controllers/index.js');
                         }]
                     }
                 })
-                .state('default.employee.employee-view', {
-                    url: '/employee-view',
-                    templateUrl: 'modules/employee/views/employee-view.html',
-                    controller: 'EmployeeEmployeeViewCtrl',
+                .state('default.mod1.customers-i-guess', {
+                    url: '/customers-i-guess',
+                    templateUrl: 'modules/mod-1/views/customers-i-guess.html',
+                    controller: 'Mod1CustomersIGuessCtrl',
                     data: {
-                        ensureJsdos: ["SSOwhTwo"]
+                        ensureJsdos: []
+                    }
+                })
+                .state('default.mod1.cust', {
+                    url: '/cust',
+                    templateUrl: 'modules/mod-1/views/cust.html',
+                    controller: 'Mod1CustCtrl',
+                    data: {
+                        ensureJsdos: ["test"]
                     }
                 })
                 .state('login', {
